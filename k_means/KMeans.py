713f9f40dfd7f8c2b065e77783b89299213fd543
toto-haricot <t.waldura@yahoo.com>
2022-07-29 19:06:24 +0200

    keep building kmeans script

--- k_means/KMeans.py
+++ k_means/KMeans.py
@@ -20,9 +20,44 @@ class KMeans():
         # random select K points
         random_idx = random.sample(range(n), K)
         centroids = X_train[random_idx]
+        predictions = self.give_class(X_train, centroids)
+
+        keep_going = True
+
+        while keep_going:
+
+            dataset = np.hstack([X_train, predictions])
+            centroids = self.search_centroids(dataset)
+            new_predictions = self.give_class(X_train, centroids)
+
+            if changes(predictions, new_predictions) == 0: keep_going=False
+
+            predictions = new_predictions
+
+        return(predictions)
 
 
-    def give_class(self, X_train:np.array, K_centroids:np.array):
+
+
+    def search_centroids(self, X_:np.array):
+
+        n, d = X_.shape
+        classes = np.uniques(X_[:,-1])
+        new_centroids = np.zeros((self.K, d-1))
+
+        for i,c in enumerate(classes):
+            
+            subset = X_[X_[:,-1] == c]
+            centroid = np.mean(subset, axis=0)
+            new_centroids[i] = centroid
+
+        return new_centroids
+
+
+
+
+    @staticmethod
+    def give_class(X_train:np.array, K_centroids:np.array):
         """For each data point of X_train we compute the closest centroid based on
         Euclidean distance metrics
 
@@ -35,14 +70,22 @@ class KMeans():
         K = len(K_centroids)
 
         X_train_3d = np.reshape(X_train, (n, 1, d))
-        X_train_3d = np.repeat(X_train_3d, repeats=K)
+        X_train_3d = np.repeat(X_train_3d, repeats=K, axis=1)
 
-        norm = np.abs(X_train - K_centroids)**2
+        norm = np.abs(X_train_3d - K_centroids)**2
         norm = np.sum(norm, axis=2)
 
         pred = np.argmin(norm, axis=1)
+        pred = np.reshape(pred, (n,1))
 
         return(pred)
+
+    
+    @staticmethod
+    def changes(new_classes:np.array, old_classes:np.array):
+
+        return(np.sum(new_classes == old_classes))
+
 
 
 
