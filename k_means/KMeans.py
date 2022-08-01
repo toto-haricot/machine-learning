aeb3bce2f8d9b1056cc8a7de0a4893fab66af91f
toto-haricot <t.waldura@yahoo.com>
2022-08-01 21:21:00 +0200

    fix KMeans.normalize errors

--- k_means/KMeans.py
+++ k_means/KMeans.py
@@ -20,41 +20,39 @@ class KMeans():
         Args:
             X_train (np.array): training dataset
             K (int): number of clusters to look for
+            display (Bool): True or False if you want to plot the clusters for each
+                            iteration of K-Means
         """
         
         self.K = K
-        n, d = X_train.shape
         n_epoch = 1
+        keep_going = True
+        n, d = X_train.shape
+        X_train = self.normalize(X_train)
 
-        # random select K points
+        if d > 3: display = False
+
+        # random select K points as first centroids
         random_idx = random.sample(range(n), K)
         initial_centroids = X_train[random_idx]
-
         self.centroids = initial_centroids
 
         predictions = self.give_class(X_train, initial_centroids)
-        dataset = np.hstack([X_train, predictions])
-
-        keep_going = True
 
         while keep_going:
 
             dataset = np.hstack([X_train, predictions])
-
+            # compute new centroids coordinates
             centroids = self.search_centroids(dataset)
             self.centroids = centroids
+            # find new clusters
             new_predictions = self.give_class(X_train, centroids)
-
             new_dataset = np.hstack([X_train, new_predictions])
+            # compute number of data points changed class
             changes = self.changes(predictions, new_predictions)
-
-            if display:
-                plt.figure(figsize=(12, 7))
-                plt.title(f"Epoch {n_epoch-1}")
-                plt.scatter(x=dataset[:,0], y=dataset[:,1], c=dataset[:,2])
-                plt.scatter(x=centroids[:,0], y=centroids[:,1], c='r')
-                plt.show()
-
+            # plotting clustering evolution
+            if display: self.plot_clusters(dataset, centroids, dim=d, epoch=n_epoch)
+            # checking if optimum found
             if (n_epoch >= n_epochs_max) or (changes == 0): keep_going=False
 
             predictions = new_predictions
@@ -121,6 +119,47 @@ class KMeans():
             int: number of different values
         """
         return(np.sum(new_classes != old_classes))
+
+    
+    @staticmethod
+    def plot_clusters(dataset:np.array, centroids:np.array, dim:int, fig_size=(12,7), epoch=None):
+
+        n, d = dataset.shape
+
+        if d == 3:
+
+            plt.figure(figsize=fig_size)
+            plt.title(f"Epoch {epoch-1}")
+            plt.scatter(x=dataset[:,0], y=dataset[:,1], c=dataset[:,2])
+            plt.scatter(x=centroids[:,0], y=centroids[:,1], c='r')
+            plt.show()
+
+        if d == 4: 
+
+            plt.figure(figsize=fig_size)
+            ax = plt.axes(projection='3d')
+            ax.set_title(f"Epoch {epoch-1}")
+            ax.scatter(xs=dataset[:,0], ys=dataset[:,1], zs=dataset[:,2], c=dataset[:,3])
+            plt.show()
+
+    @staticmethod
+    def normalize(dataset:np.array):
+
+        n, d = dataset.shape
+        dataset_norm = np.zeros((n, d))
+
+        for i in range(d):
+
+            mean, std = dataset[:,i].mean(), dataset[:,i].std()
+            dataset_norm[:,i] = (dataset[:,i] - mean) / std
+
+        return(dataset_norm)
+
+
+
+
+
+
 
 
 
